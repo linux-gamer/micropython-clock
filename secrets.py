149d39003cc4e7245cb70b284479c15b73bffec2
linux-gamer <115344588+linux-gamer@users.noreply.github.com>
2022-11-26 17:27:03 +0100

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,7 @@
+openweathermap_api_key = "Get your api key from https://openweathermap.org/api."
+
+wlan_ssid = "YOUR_WLAN_SSID"
+wlan_pwd = "YOUR_WLAN_PASSWORD"
+
+lat = 0 # your location latitude
+lon = 0 # your location longitude
